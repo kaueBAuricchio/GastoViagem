d29c5094b8044d485348ba93887e8330a024de83
kaueBAuricchio <46527280+kaueBAuricchio@users.noreply.github.com>
2022-06-10 20:47:43 -0300

    activity_main.xml finish the app

--- app/src/main/java/com/example/gastoviagem/MainActivity.kt
+++ app/src/main/java/com/example/gastoviagem/MainActivity.kt
@@ -2,10 +2,45 @@ package com.example.gastoviagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.gastoviagem.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+      private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ButtonCalculate.setOnClickListener{
+            calculate()
+        }
+
+    }
+
+    private fun calculate(){
+
+        if (validate()) {
+
+            val distance = binding.EditTextKm.text.toString().toFloat()
+            val price = binding.EditTextPrice.text.toString().toFloat()
+            val autonomy = binding.EditTextAutonomy.text.toString().toFloat()
+            val totalValue = (distance * price) / autonomy
+
+            binding.TextViewValue.text = "R$ ${"%.2f"}".format(totalValue)
+
+        } else {
+
+            Toast.makeText(this,R.string.validate_fields,Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun validate() : Boolean{
+        return (binding.EditTextKm.text.toString() !=""
+                && binding.EditTextPrice.text.toString() != ""
+                && binding.EditTextAutonomy.text.toString() != ""
+                && binding.EditTextAutonomy.text.toString().toFloat() != 0f)
     }
 }
